a13d2b80c48cd84d5f774ae6ec7cabf498fd0c98
olidelaune <odelaune27@holynames-sea.org>
2026-03-18 16:34:02 -0700

    added distance to smart dashboard

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -21,6 +21,7 @@ public class Limelight extends SubsystemBase {
   private int m_targetID = -1;
   private int m_desiredTagID = -1;  // -1 means any tag
   private double m_targetArea = -1;
+  private double m_distance =-1;
  
   private PoseEstimate m_latestPoseEstimate = new PoseEstimate();
 
@@ -49,6 +50,7 @@ public class Limelight extends SubsystemBase {
     SmartDashboard.putNumber(Constants.LimelightConstants.kDashboardDistanceKey, 0.0);
     SmartDashboard.putNumber(Constants.LimelightConstants.kDashboardHorizontalDistanceKey, 0.0);
     SmartDashboard.putNumber(Constants.LimelightConstants.kDashboardTargetAreaKey, 0.0);
+    SmartDashboard.putNumber(Constants.LimelightConstants.kDashboardDistanceKey, 0.0);
     
   }
 
@@ -88,6 +90,7 @@ public class Limelight extends SubsystemBase {
       m_targetOffsetX = primaryFiducial.txnc;
       m_targetOffsetY = primaryFiducial.tync;
       m_targetArea = LimelightHelpers.getTA(m_limelightName); 
+      m_distance = getDistanceToTarget();
       
       
       // If desired tag is set, check if this is the one we want
@@ -99,6 +102,7 @@ public class Limelight extends SubsystemBase {
             m_targetOffsetX = fid.txnc;
             m_targetOffsetY = fid.tync;
             m_targetArea = fid.ta; 
+            m_distance = getDistanceToTarget();
             break;
           }
         }
@@ -109,6 +113,7 @@ public class Limelight extends SubsystemBase {
       m_targetOffsetX = 0.0;
       m_targetOffsetY = 0.0;
       m_targetArea = 0.0;
+      m_distance = 0.0;
       
     }
   }
@@ -123,6 +128,7 @@ public class Limelight extends SubsystemBase {
     SmartDashboard.putNumber(Constants.LimelightConstants.kDashboardOffsetXKey, m_targetOffsetX);
     SmartDashboard.putNumber(Constants.LimelightConstants.kDashboardOffsetYKey, m_targetOffsetY);
     SmartDashboard.putNumber(Constants.LimelightConstants.kDashboardTargetAreaKey, m_targetArea);
+    SmartDashboard.putNumber(Constants.LimelightConstants.kDashboardDistanceKey, m_distance);
   
     double[] poseRelative = getRobotPoseRelativeToTarget();
     if (poseRelative != null && poseRelative.length >= 2) {
@@ -211,6 +217,7 @@ public class Limelight extends SubsystemBase {
   public double getTargetArea() {
     return LimelightHelpers.getTA(m_limelightName);
   }
+
 
   /**
    * Get pipeline latency in milliseconds
@@ -334,7 +341,8 @@ public class Limelight extends SubsystemBase {
         m_targetOffsetX, 
         m_targetOffsetY, 
         distance,
-        getTargetArea()
+        getTargetArea(),
+        m_distance
     );
   }
 }
